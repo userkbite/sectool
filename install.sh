29594d20ef37b3df1a625d4dd54e6bed15cfcdcf
Nightzx <69611562+userkbite@users.noreply.github.com>
2021-12-26 07:22:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ apt-get upgrade
 apt-get install -y  python3
 pip install requests
 
-echo "[i] all dependencies have already been installed, run the command \"python3 secdet.py\" to run the script"
+echo "[i] all dependencies have already been installed, run the command \"python3 sectool.py\" to run the script"
